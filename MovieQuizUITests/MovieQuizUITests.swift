aae07e9185820b197429a69f2595322a3db86b25
teenagelove <dani4upa586@gmail.com>
2024-11-30 21:02:03 +0900

    Add UI testGameResultAlert

--- MovieQuizUITests/MovieQuizUITests.swift
+++ MovieQuizUITests/MovieQuizUITests.swift
@@ -18,17 +18,55 @@ final class MovieQuizUITests: XCTestCase {
         app.terminate()
         app = nil
     }
-
-    func testScreenCast() throws {
-        let iterator = 1...10
-        let noButton = app.buttons["Нет"]
+    
+    func testGameResultAlert() {
+        let noButton = app.buttons["No"]
+        let alert = app.alerts["Этот раунд окончен!"]
+        let index = app.staticTexts["Index"]
         
-        iterator.forEach { _ in
-            sleep(1)
+        for iter in 1...10 {
+            sleep(2)
+            XCTAssertEqual(index.label, "\(iter)/10")
             noButton.tap()
         }
         
-        app.alerts["Этот раунд окончен!"].scrollViews.otherElements.buttons["Сыграть еще раз"].tap()
+        sleep(1)
+        XCTAssertTrue(alert.exists)
+        XCTAssertEqual(alert.label, "Этот раунд окончен!")
+        XCTAssertEqual(alert.buttons.firstMatch.label, "Сыграть еще раз")
         
+        alert.buttons.firstMatch.tap()
+        sleep(1)
+        
+        XCTAssertFalse(alert.exists)
+        XCTAssertEqual(index.label, "1/10")
+    }
+
+    func testYesButton() {
+        sleep(1)
+        let firstPoster = app.images["Poster"]
+        let firstPosterData = firstPoster.screenshot().pngRepresentation
+        
+        app.buttons["Yes"].tap()
+        sleep(1)
+        
+        let secondPoster = app.images["Poster"]
+        let secondPosterData = secondPoster.screenshot().pngRepresentation
+        
+        XCTAssertNotEqual(firstPosterData, secondPosterData)
+    }
+    
+    func testNoButton() {
+        sleep(1)
+        let firstPoster = app.images["Poster"]
+        let firstPosterData = firstPoster.screenshot().pngRepresentation
+        
+        app.buttons["No"].tap()
+        sleep(1)
+        
+        let secondPoster = app.images["Poster"]
+        let secondPosterData = secondPoster.screenshot().pngRepresentation
+        
+        XCTAssertNotEqual(firstPosterData, secondPosterData)
     }
 }
